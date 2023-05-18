a71944fe32d44bca1e4628377cf8d6d933542114
박소예 <soye@bagsoyeui-MacBookAir.local>
2023-05-18 21:10:19 +0900

    팝업스토어 웹사이트반응형 hash 시작

---
+++ js/index.js
@@ -0,0 +1,100 @@
+
+const popup_header = document.querySelector("header");
+
+window.addEventListener("scroll" , () => {
+
+ var $scrollY = window.scrollY;
+
+ if($scrollY  > 0){
+
+    popup_header.classList.add("active");
+
+ }else{
+
+    popup_header.classList.remove("active");
+ }
+ 
+})
+
+
+
+let slider_wrap = document.querySelector(".slider_wrap");
+const slider_prevBtn = document.querySelector(".prevBtn");
+const slider_nextBtn = document.querySelector(".nextBtn");
+
+function popup_auto(){ 
+    
+    setInterval( (evt) => {
+
+    const $hover = slider_wrap.classList.contains("hover");
+
+    if($hover == true){
+
+
+    }else{
+
+    const slides_first = document.querySelector(".slider_wrap li:last-child");
+
+    slider_wrap.classList.add("active");
+
+    setTimeout( function() {
+
+        slider_wrap.prepend(slides_first);
+        slider_wrap.classList.remove("active");
+
+    },1000)
+
+    }
+
+},3000)
+
+}
+
+popup_auto();
+
+
+slider_wrap.addEventListener("mouseover" , (ev) => {
+
+    const $hover = slider_wrap.classList.add("hover");
+
+    slider_prevBtn.addEventListener("click" , () => {
+
+        const slides_last = document.querySelector(".slider_wrap li:first-child");
+    
+        slider_wrap.classList.add("move");
+    
+        setTimeout( function() {
+    
+            slider_wrap.append(slides_last);
+            slider_wrap.classList.remove("move");
+    
+        },1000)
+    
+    
+    })
+
+    slider_nextBtn.addEventListener("click" , () => {
+
+        const slides_first = document.querySelector(".slider_wrap li:last-child");
+        console.log(slides_first);
+    
+        slider_wrap.classList.add("active");
+    
+        setTimeout( function() {
+    
+            slider_wrap.prepend(slides_first);
+            slider_wrap.classList.remove("active");
+    
+        },1000)
+    
+    
+    })
+
+})
+
+
+slider_wrap.addEventListener("mouseout" , () => {
+
+    const $hover = slider_wrap.classList.remove("hover");
+
+})
